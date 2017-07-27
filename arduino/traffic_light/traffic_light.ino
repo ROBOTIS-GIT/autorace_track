99bc57c0fefd79d89b8c6e737a60907f73122f14
Leon Ryuwoon Jung <rwjung@robotis.com>
2017-07-27 21:38:40 +0900

    first upload

--- arduino/traffic_light/traffic_light.ino
+++ arduino/traffic_light/traffic_light.ino
@@ -1,9 +1,14 @@
+#include <OLLO.h>
+OLLO myOLLO;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(3, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+
+  myOLLO.begin(1);//DMS Module must be connected at port 1.
+  myOLLO.begin(2);//DMS Module must be connected at port 2.
 }
 
 void loop() {
@@ -21,4 +26,11 @@ void loop() {
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
   delay(500);    
+
+  Serial.print("DMS Sensor 1 ADC Value = ");
+  Serial.println(myOLLO.read(1)); //read ADC value from OLLO port 1
+  delay(60);
+  Serial.print("DMS Sensor 2 ADC Value = ");
+  Serial.println(myOLLO.read(2)); //read ADC value from OLLO port 2
+  delay(60);
 }
